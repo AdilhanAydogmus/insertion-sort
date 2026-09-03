d465101a577b8353be579b9284992b22d2472c18
Adilhan <125409095+AdilhanAydogmus@users.noreply.github.com>
2026-09-03 21:59:04 +0300

    Add files via upload

---
+++ insertion_sort.py
@@ -0,0 +1,24 @@
+import time
+
+def insertion_sort(dataInput):
+    data = dataInput.copy()
+    start = time.perf_counter()  
+    for i in range(1, len(data)):
+        current = data[i]
+        j = i-1
+
+        while j >= 0 and data[j] > current:
+            data[j+1] = data[j]
+            j -= 1
+        data[j+1] = current
+
+    end = time.perf_counter()
+    process_time = end - start
+    return data, f"\nThis process took {process_time:.6f} seconds."
+
+
+if __name__ == "__main__":
+    import random
+    sample_data = [random.randint(1, 10_000) for _ in range(1_000)]
+    sorted_data, duration = insertion_sort(sample_data)
+    print(f"Sorted {len(sorted_data)} items in {duration:.6f} seconds.")
